8a21b6389b07e6d246d8ba1473de5758146fd091
18juba <jubalucas05@gmail.com>
2025-09-23 10:38:44 -0300

    Header positions

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,29 +8,33 @@ type HeaderProps = {
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar, isOpen }) => {
   return (
-    <header className="w-full py-2 bg-[var(--secondary)] text-[var(--extra)] shadow-md z-50 relative ">
-      <button
-        onClick={toggleSidebar}
-        className="absolute left-0 top-1/2 -translate-y-1/2 px-4 cursor-pointer "
-      >
-        {isOpen ? (
-          <img src="/icons/close.svg" alt="menu" className="w-8 h-8" />
-        ) : (
-          <img src="/icons/open.svg" alt="menu" className="w-7 h-7" />
-        )}
-      </button>
+    <header className="relative flex justify-center items-center w-full bg-[var(--secondary)] text-[var(--secondary-foreground)] shadow-md z-50">
+      <div className="flex items-center justify-center border-r px-12 py-3 border-[var(--secondary-foreground)]">
+        <button onClick={toggleSidebar} className="cursor-pointer">
+          {isOpen ? (
+            <img src="/icons/close.svg" alt="menu" className="w-10 h-10" />
+          ) : (
+            <img src="/icons/open.svg" alt="menu" className="w-10 h-10" />
+          )}
+        </button>
+      </div>
 
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3 pl-16">
-        <Link href="/" className="hover:text-[var(--secondary)] transition">
-          Início
-        </Link>
+      <div className="w-full mx-auto flex items-center justify-between px-8">
+        <div className="flex gap-2 items-center">
+          <img src="/icons/config.svg" alt="config" className="w-8 h-8" />
+          <div>
+            <h1 className="font-bold">Empresa</h1>
+            <p className="text-xs">CNPJ: 00</p>
+          </div>
+          <img src="/icons/config.svg" alt="config" className="ml-4 w-4 h-4" />
+        </div>
 
         <div className="flex items-center gap-6">
           <Link href="/sobre">
-            <img src="/icons/config.svg" alt="config" className="w-7 h-7" />
+            <img src="/icons/config.svg" alt="config" className="w-6 h-6" />
           </Link>
           <Link href="/notificacoes">
-            <img src="/icons/bell.svg" alt="bell" className="w-7 h-7" />
+            <img src="/icons/bell.svg" alt="bell" className="w-6 h-6" />
           </Link>
         </div>
 
@@ -38,8 +42,9 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar, isOpen }) => {
           href="/perfil"
           className="flex items-center gap-2 hover:text-[var(--secondary)] transition"
         >
-          <img src="/icons/userIcon.svg" alt="user" className="w-6 h-6" />
-          Perfil
+          <img src="/icons/userIcon.svg" alt="user" className="w-10 h-10" />
+          <h1 className="font-bold">Eu sou o Usuário</h1>
+          <img src="/icons/userIcon.svg" alt="user" className="ml-2 w-4 h-4" />
         </Link>
       </div>
     </header>
