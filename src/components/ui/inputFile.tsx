23c9641dda2c4c3db45120c19852faf3453f82b5
18juba <jubalucas05@gmail.com>
2025-09-30 10:10:45 -0300

    Input file com animação

--- src/components/ui/inputFile.tsx
+++ src/components/ui/inputFile.tsx
@@ -26,12 +26,14 @@ const InputFile: React.FC<InputFileProps> = ({
   label = "Escolher arquivo",
 }) => {
   const [loading, setLoading] = useState(false);
+  const [uploaded, setUploaded] = useState(false); // 👈 novo estado
 
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
 
     const selectedFiles = Array.from(e.target.files);
     setLoading(true);
+    setUploaded(false);
 
     try {
       const uploadedFiles: UploadedFile[] = [];
@@ -62,6 +64,10 @@ const InputFile: React.FC<InputFileProps> = ({
       }
 
       if (onUpload) onUpload(uploadedFiles);
+
+      setUploaded(true);
+
+      setTimeout(() => setUploaded(false), 1500);
     } catch (error) {
       console.error(error);
     } finally {
@@ -85,8 +91,24 @@ const InputFile: React.FC<InputFileProps> = ({
         htmlFor={id}
         className="flex items-center gap-2 cursor-pointer bg-[var(--primary)] text-[var(--extra)] px-4 py-2 rounded-md text-lg hover:opacity-90 transition"
       >
-        <div className="bg-[var(--secondary)] text-[var(--extra)] p-2 rounded">
-          <img src="/Icons/UploadFile.svg" alt="" className="w-8 h-8" />
+        <div className="bg-[var(--secondary)] text-[var(--extra)] p-1 rounded">
+          {loading ? (
+            <img
+              src="/Icons/UploadFile.svg"
+              alt=""
+              className="w-9 h-9 animate-pulse"
+            />
+          ) : uploaded ? (
+            <video
+              src="/Videos/FileUploaded.webm"
+              autoPlay
+              muted
+              playsInline
+              className="w-9 h-9"
+            />
+          ) : (
+            <img src="/Icons/UploadFile.svg" alt="" className="w-9 h-9" />
+          )}
         </div>
         {loading ? "Enviando..." : label}
       </label>
